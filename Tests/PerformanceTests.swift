d08217a1472a673c37449ceeb23b8a4ecabec3de
Nick Lockwood <nick@charcoaldesign.co.uk>
2017-11-24 16:00:14 +0000

    Added additional performance tests

--- Tests/PerformanceTests.swift
+++ Tests/PerformanceTests.swift
@@ -43,6 +43,38 @@ class PerformanceTests: XCTestCase {
         }
     }
 
+    func testWorstCaseFormatting() {
+        let files = PerformanceTests.files
+        let tokens = files.map { tokenize($0) }
+        let options = FormatOptions(
+            linebreak: "\r\n",
+            spaceAroundRangeOperators: false,
+            spaceAroundOperatorDeclarations: false,
+            useVoid: false,
+            indentCase: true,
+            trailingCommas: false,
+            indentComments: false,
+            truncateBlankLines: false,
+            allmanBraces: true,
+            ifdefIndent: .outdent,
+            wrapArguments: .beforeFirst,
+            wrapElements: .afterFirst,
+            uppercaseHex: false,
+            uppercaseExponent: true,
+            decimalGrouping: .group(1, 1),
+            binaryGrouping: .group(1, 1),
+            octalGrouping: .group(1, 1),
+            hexGrouping: .group(1, 1),
+            hoistPatternLet: false,
+            elseOnNextLine: true,
+            removeSelf: false,
+            experimentalRules: true
+        )
+        measure {
+            _ = tokens.map { try! format($0, rules: FormatRules.default, options: options) }
+        }
+    }
+
     func testInferring() {
         let files = PerformanceTests.files
         let tokens = files.flatMap { tokenize($0) }
@@ -59,6 +91,15 @@ class PerformanceTests: XCTestCase {
         }
     }
 
+    func testWorstCaseIndent() {
+        let files = PerformanceTests.files
+        let tokens = files.map { tokenize($0) }
+        let options = FormatOptions(indent: "\t", allmanBraces: true)
+        measure {
+            _ = tokens.map { try! format($0, rules: [FormatRules.indent], options: options) }
+        }
+    }
+
     func testRedundantSelf() {
         let files = PerformanceTests.files
         let tokens = files.map { tokenize($0) }
@@ -67,11 +108,36 @@ class PerformanceTests: XCTestCase {
         }
     }
 
+    func testWorstCaseRedundantSelf() {
+        let files = PerformanceTests.files
+        let tokens = files.map { tokenize($0) }
+        let options = FormatOptions(removeSelf: false)
+        measure {
+            _ = tokens.map { try! format($0, rules: [FormatRules.redundantSelf], options: options) }
+        }
+    }
+
     func testNumberFormatting() {
         let files = PerformanceTests.files
         let tokens = files.map { tokenize($0) }
         measure {
             _ = tokens.map { try! format($0, rules: [FormatRules.numberFormatting]) }
+        }
+    }
+
+    func testWorstCaseNumberFormatting() {
+        let files = PerformanceTests.files
+        let tokens = files.map { tokenize($0) }
+        let options = FormatOptions(
+            uppercaseHex: false,
+            uppercaseExponent: true,
+            decimalGrouping: .group(1, 1),
+            binaryGrouping: .group(1, 1),
+            octalGrouping: .group(1, 1),
+            hexGrouping: .group(1, 1)
+        )
+        measure {
+            _ = tokens.map { try! format($0, rules: [FormatRules.numberFormatting], options: options) }
         }
     }
 }

f7dfe1cb822a8fb38b7320a9333cb3d5ae0fce64
Vince Burn <vincentbernier@hotmail.com>
2018-01-28 01:16:34 -0500

    Basic TableView Setup

---
+++ EditorExtension/Application/Source/RuleSelectionTableCellView.swift
@@ -0,0 +1,21 @@
+//
+//  RuleSelectionTableCellView.swift
+//  SwiftFormat for Xcode
+//
+//  Created by Vincent Bernier on 28-01-18.
+//  Copyright © 2018 Nick Lockwood. All rights reserved.
+//
+
+import Cocoa
+
+class RuleSelectionTableCellView: NSTableCellView {
+
+    @IBOutlet var button: NSButton!
+    //    override func draw(_ dirtyRect: NSRect) {
+//        super.draw(dirtyRect)
+//
+//        // Drawing code here.
+//    }
+
+    
+}

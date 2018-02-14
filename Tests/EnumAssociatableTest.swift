f4a52e520d90a6af33f2d8e0d2f95c3231cdeb54
Vince Burn <vincentbernier@hotmail.com>
2018-02-13 23:29:43 -0500

    Add EnumAssociatable protocol

---
+++ Tests/EnumAssociatableTest.swift
@@ -0,0 +1,14 @@
+//
+//  EnumAssociatableTest.swift
+//  SwiftFormat
+//
+//  Created by Vincent Bernier on 13-02-18.
+//  Copyright © 2018 Nick Lockwood.
+//
+
+import XCTest
+@testable import SwiftFormat
+
+class EnumAssociatableTest: XCTestCase {
+
+}

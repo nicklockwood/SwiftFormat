c3fcffee37bfb23b1098f630cc30c5360d9da9f8
Vince Burn <vincentbernier@hotmail.com>
2018-02-25 08:07:17 -0500

    Update file count test

--- Tests/SwiftFormatTests.swift
+++ Tests/SwiftFormatTests.swift
@@ -68,7 +68,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 32)
+        XCTAssertEqual(files.count, 33)
     }
 
     func testInputFilesMatchOutputFilesForSameOutput() {
@@ -79,7 +79,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 32)
+        XCTAssertEqual(files.count, 33)
     }
 
     func testInputFileNotEnumeratedWhenExcluded() {
@@ -92,7 +92,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 23)
+        XCTAssertEqual(files.count, 24)
     }
 
     // MARK: format function

00bf5d3c063f99bfeb7da168a293fec26403515e
Vince Burn <vincentbernier@hotmail.com>
2018-03-05 20:46:46 -0500

    Add Save, Open, Reset
    Save the config to a file
    Open (load) a config from a file
    Reset the config to the defaults values

--- EditorExtension/Application/Source/AppDelegate.swift
+++ EditorExtension/Application/Source/AppDelegate.swift
@@ -31,5 +31,138 @@
 
 import Cocoa
 
+struct Version: Codable {
+    let version: Int
+}
+
+struct SwiftFormatXcodeConfiguration: Codable {
+    static let fileExtension = "sfxx" //  TODO: Define the official extension
+
+    let version: Int
+    let rules: [Rule]
+    let options: [SavedOption]
+}
+
+enum XcodeConfigurationError: Error, CustomStringConvertible, EnumAssociatable {
+    case writing(String)
+    case reading(String)
+    case parsing(String)
+
+    var description: String {
+        let message: String = associatedValue()
+        return message
+    }
+
+    var localizedDescription: String {
+        return description
+    }
+}
+
+extension NSNotification.Name {
+    static let ApplicationDidLoadNewConfiguration = NSNotification.Name("ApplicationDidLoadNewConfiguration")
+}
+
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {}
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow? {
+        return NSApp.mainWindow
+    }
+
+    @objc
+    @IBAction func resetToDefault(_: NSMenuItem) {
+        RulesStore().resetRulesToDefaults()
+        OptionsStore().resetOptionsToDefaults()
+        NotificationCenter.default.post(name: .ApplicationDidLoadNewConfiguration, object: nil)
+    }
+
+    @objc
+    @IBAction func openConfiguration(_: NSMenuItem) {
+        guard let window = window else {
+            return
+        }
+
+        let dialog = NSOpenPanel()
+        dialog.title = "Choose a configuration file"
+        dialog.showsResizeIndicator = true
+        dialog.allowedFileTypes = [SwiftFormatXcodeConfiguration.fileExtension]
+        dialog.allowsMultipleSelection = false
+        print(NSImage.imageTypes)
+
+        dialog.beginSheetModal(for: window) { response in
+            guard response == .OK, let url = dialog.url else {
+                return
+            }
+
+            let data: Data
+            do {
+                data = try Data(contentsOf: url)
+            } catch let error {
+                self.showError(XcodeConfigurationError.reading("Problem while reading the file \(url). [\(error)]"))
+                return
+            }
+
+            let decoder = JSONDecoder()
+            do {
+                let version = try decoder.decode(Version.self, from: data)
+                if version.version != 1 {
+                    throw XcodeConfigurationError.parsing("Unsupported version number: \(version.version)")
+                }
+                let configuration = try decoder.decode(SwiftFormatXcodeConfiguration.self, from: data)
+                RulesStore().restore(configuration.rules)
+                OptionsStore().restore(configuration.options)
+
+                NotificationCenter.default.post(name: .ApplicationDidLoadNewConfiguration, object: nil)
+            } catch let error as XcodeConfigurationError {
+                self.showError(error)
+            } catch let error {
+                self.showError(XcodeConfigurationError.parsing("Problem while decoding file: \(url). [\(error)]"))
+            }
+        }
+    }
+
+    @objc
+    @IBAction func saveConfiguration(_: NSMenuItem) {
+        guard let window = window else {
+            return
+        }
+
+        let conf = SwiftFormatXcodeConfiguration(version: 1,
+                                                 rules: RulesStore().rules,
+                                                 options: OptionsStore().options)
+
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let dataToWrite: Data
+        do {
+            dataToWrite = try encoder.encode(conf)
+        } catch let error {
+            self.showError(XcodeConfigurationError.writing("Problem while encoding configuration data. [\(error)]"))
+            return
+        }
+
+        let dialog = NSSavePanel()
+        dialog.title = "Export Configuration"
+        dialog.nameFieldStringValue = "name.\(SwiftFormatXcodeConfiguration.fileExtension)"
+        dialog.beginSheetModal(for: window) { response in
+            guard response == .OK, let url = dialog.url else {
+                return
+            }
+            do {
+                try dataToWrite.write(to: url)
+            } catch let error {
+                self.showError(XcodeConfigurationError.writing("Problem while writing configuration to url: \(url). [\(error)]"))
+            }
+        }
+    }
+
+    private func showError(_ error: Error) {
+        guard let window = window else {
+            return
+        }
+
+        let alert = NSAlert(error: error)
+        alert.addButton(withTitle: "OK")
+        alert.alertStyle = .critical
+        alert.beginSheetModal(for: window)
+    }
+}

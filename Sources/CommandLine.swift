6c5c0cb943fedf4b2d67f4926083703c396347b5
Nick Lockwood <nick@charcoaldesign.co.uk>
2018-07-31 13:43:33 +0100

    Fixed verbose mode reporting wrong rules applied

--- Sources/CommandLine.swift
+++ Sources/CommandLine.swift
@@ -570,7 +570,8 @@ func format(_ source: String,
 
     // Apply rules
     let rulesByName = FormatRules.byName
-    let rules = ruleNames.sorted().compactMap { rulesByName[$0] }
+    let ruleNames = ruleNames.sorted()
+    let rules = ruleNames.compactMap { rulesByName[$0] }
     var rulesApplied = Set<String>()
     let callback: ((Int, [Token]) -> Void)? = verbose ? { i, updatedTokens in
         if updatedTokens != tokens {

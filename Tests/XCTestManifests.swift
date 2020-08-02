e3a92cad79838f281a50a55a5b0d9d7fff65016f
facumenzella <facumenzella@gmail.com>
2020-08-02 15:56:14 +0200

    Running tests

--- Tests/XCTestManifests.swift
+++ Tests/XCTestManifests.swift
@@ -884,6 +884,7 @@ extension RulesTests {
         ("testMultilineCommentHeader", testMultilineCommentHeader),
         ("testMultilineDisableNextRemoveSelf", testMultilineDisableNextRemoveSelf),
         ("testMultilineDisableRemoveSelf", testMultilineDisableRemoveSelf),
+        ("testMultilineEnumCases", testMultilineEnumCases),
         ("testMultilineForLoopBraceOnNextLine", testMultilineForLoopBraceOnNextLine),
         ("testMultilineForLoopBraceOnNextLine2", testMultilineForLoopBraceOnNextLine2),
         ("testMultilineForWhereLoopBraceOnNextLine", testMultilineForWhereLoopBraceOnNextLine),

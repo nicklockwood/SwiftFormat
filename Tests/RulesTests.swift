867210ab1c96d71ee1d84e979d0f3592d8c9e421
Facundo Menzella <facumenzella@users.noreply.github.com>
2020-09-20 18:05:36 +0200

    Enum namespace rule (#737)

--- Tests/RulesTests.swift
+++ Tests/RulesTests.swift
@@ -1204,6 +1204,237 @@ class RulesTests: XCTestCase {
         testFormatting(for: input, output, rule: FormatRules.hoistPatternLet, options: options)
     }
 
+    // MARK: - enumNamespaces
+
+    func testEnumNamespacesClassAsProtocolRestriction() {
+        let input = """
+        @objc protocol Foo: class {
+            @objc static var expressionTypes: [String: RuntimeType] { get }
+        }
+        """
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesConformingOtherType() {
+        let input = "private final class CustomUITableViewCell: UITableViewCell {}"
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesImportClass() {
+        let input = "import class MyUIKit.AutoHeightTableView"
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesImportStruct() {
+        let input = "import struct Core.CurrencyFormatter"
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesClassFunction() {
+        let input = """
+        class Container {
+            class func bar() {}
+        }
+        """
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesRemovingExtraKeywords() {
+        let input = """
+        final class MyNamespace {
+            static let bar = "bar"
+        }
+        """
+        let output = """
+        enum MyNamespace {
+            static let bar = "bar"
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesNestedTypes() {
+        let input = """
+        enum Namespace {}
+        extension Namespace {
+            struct Constants {
+                static let bar = "bar"
+            }
+        }
+        """
+        let output = """
+        enum Namespace {}
+        extension Namespace {
+            enum Constants {
+                static let bar = "bar"
+            }
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesNestedTypes2() {
+        let input = """
+        struct Namespace {
+            struct NestedNamespace {
+                static let foo: Int
+                static let bar: Int
+            }
+        }
+        """
+        let output = """
+        enum Namespace {
+            enum NestedNamespace {
+                static let foo: Int
+                static let bar: Int
+            }
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesNestedTypes3() {
+        let input = """
+        struct Namespace {
+            struct TypeNestedInNamespace {
+                let foo: Int
+                let bar: Int
+            }
+        }
+        """
+        let output = """
+        enum Namespace {
+            struct TypeNestedInNamespace {
+                let foo: Int
+                let bar: Int
+            }
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesNestedTypes4() {
+        let input = """
+        struct Namespace {
+            static func staticFunction() {
+                struct NestedType {
+                    init() {}
+                }
+            }
+        }
+        """
+        let output = """
+        enum Namespace {
+            static func staticFunction() {
+                struct NestedType {
+                    init() {}
+                }
+            }
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesNestedTypes5() {
+        let input = """
+        struct Namespace {
+            static func staticFunction() {
+                func nestedFunction() { /* ... */ }
+            }
+        }
+        """
+        let output = """
+        enum Namespace {
+            static func staticFunction() {
+                func nestedFunction() { /* ... */ }
+            }
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesStaticVariable() {
+        let input = """
+        struct Constants {
+            static let β = 0, 5
+        }
+        """
+        let output = """
+        enum Constants {
+            static let β = 0, 5
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesStaticAndInstanceVariable() {
+        let input = """
+        struct Constants {
+            static let β = 0, 5
+            let Ɣ = 0, 3
+        }
+        """
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesStaticFunction() {
+        let input = """
+        struct Constants {
+            static func remoteConfig() -> Int {
+                return 10
+            }
+        }
+        """
+        let output = """
+        enum Constants {
+            static func remoteConfig() -> Int {
+                return 10
+            }
+        }
+        """
+        testFormatting(for: input, output, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespacesStaticAndInstanceFunction() {
+        let input = """
+        struct Constants {
+            static func remoteConfig() -> Int {
+                return 10
+            }
+
+            func instanceConfig(offset: Int) -> Int {
+                return offset + 10
+            }
+        }
+        """
+
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespaceDoesNothing() {
+        let input = """
+        struct Foo {
+            #if BAR
+                func something() {}
+            #else
+                func something() {}
+            #endif
+        }
+        """
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    func testEnumNamespaceDoesNothingEmptyDeclaration() {
+        let input = """
+        class Foo {}
+        """
+        testFormatting(for: input, rule: FormatRules.enumNamespaces)
+    }
+
+    // MARK: - trailingSpace
+
+    // truncateBlankLines = true
+
     func testUnhoistSingleCaseLet() {
         let input = "if case let .foo(bar) = quux {}"
         let output = "if case .foo(let bar) = quux {}"

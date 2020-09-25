a186f93f50f476ad548543784bbe9a7333b9b75f
Nick Lockwood <nick@charcoaldesign.co.uk>
2020-09-25 14:36:03 +0100

    Avoid removing `internal` or changing `private` to `fileprivate`

--- Sources/Rules.swift
+++ Sources/Rules.swift
@@ -4988,8 +4988,10 @@ public struct _FormatRules {
                 else { return declaration }
 
                 let extensionWithUpdatedVisibility: Formatter.Declaration
-                if memberVisibility == .internal {
-                    extensionWithUpdatedVisibility = formatter.remove(.internal, from: declaration)
+                if memberVisibility == extensionVisibility ||
+                    (memberVisibility == .internal && visibilityKeyword == nil)
+                {
+                    extensionWithUpdatedVisibility = declaration
                 } else {
                     extensionWithUpdatedVisibility = formatter.add(memberVisibility, to: declaration)
                 }

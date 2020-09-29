bc388d83708e0cb8756025d1d1c0cba4b9fc424c
Cal Stephens <cal.stephens@airbnb.com>
2020-09-29 09:57:12 -0700

    Fix an issue where visibility keywords would get placed before annotations (#758)

--- Tests/XCTestManifests.swift
+++ Tests/XCTestManifests.swift
@@ -1453,6 +1453,7 @@ extension RulesTests {
         ("testPerformBatchUpdatesNotMadeTrailing", testPerformBatchUpdatesNotMadeTrailing),
         ("testPInExponentialNotConvertedToLower", testPInExponentialNotConvertedToLower),
         ("testPInExponentialNotConvertedToUpper", testPInExponentialNotConvertedToUpper),
+        ("testPlacesVisibilityKeywordAfterAnnotations", testPlacesVisibilityKeywordAfterAnnotations),
         ("testPlacingCustomDeclarationsBeforeMarks", testPlacingCustomDeclarationsBeforeMarks),
         ("testPostfixExpressionNonYodaCondition", testPostfixExpressionNonYodaCondition),
         ("testPostfixExpressionNonYodaCondition2", testPostfixExpressionNonYodaCondition2),

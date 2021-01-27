ebbecabc414a8af596339ebde7555d8b3180982d
a.baranouski <aliaksandr.baranouski@kiwi.com>
2021-01-27 22:01:45 +0100

    `--conditionswrap` option to format condition in Xcode 12 style, in case it's too long or multiline

--- Sources/Examples.swift
+++ Sources/Examples.swift
@@ -1163,6 +1163,17 @@ private struct Examples {
     +   quuz
     + ]
     ```
+
+    `--conditionswrap auto`:
+
+    ```diff
+    - guard let foo = foo, let bar = bar, let third = third
+    + guard let foo = foo,
+    +       let bar = bar,
+    +       let third = third
+      else {}
+    ```
+
     """
 
     let wrapMultilineStatementBraces = """

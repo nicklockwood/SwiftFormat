20a0f14570a8370a5155f0df6c801a1f630be546
Cal Stephens <cal.stephens@airbnb.com>
2021-05-29 23:31:15 -0700

    Add rule to capitalize acronyms (#943)

--- Tests/RulesTests+Wrapping.swift
+++ Tests/RulesTests+Wrapping.swift
@@ -2823,7 +2823,7 @@ extension RulesTests {
             enum CodingKeys: String, CodingKey {
                 case name
                 case type
-                case categoryId = "category_id"
+                case categoryID = "category_id"
                 case attributes
             }
         }

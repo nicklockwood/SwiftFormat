134cbf8b417d0e5c96686d0f3ac794775b166558
Nick Lockwood <nick@charcoaldesign.co.uk>
2021-09-28 15:25:10 +0100

    Add `preferDouble` rule

--- Sources/Rules.swift
+++ Sources/Rules.swift
@@ -5854,4 +5854,17 @@ public struct _FormatRules {
             }
         }
     }
+
+    public let preferDouble = FormatRule(
+        help: """
+        Replaces occurrences of CGFloat with Double when targeting Swift 5.5 and above.
+        """,
+        disabledByDefault: false
+    ) { formatter in
+        guard formatter.options.swiftVersion >= "5.5" else { return }
+
+        formatter.forEach(.identifier("CGFloat")) { index, _ in
+            formatter.replaceToken(at: index, with: .identifier("Double"))
+        }
+    }
 }

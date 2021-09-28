294a9af7123aaa69881017ab8fcb8b3a244fb025
Cal Stephens <cal@calstephens.tech>
2021-09-28 18:38:57 +0100

    blockToLineComments updates

--- Tests/RulesTests.swift
+++ Tests/RulesTests.swift
@@ -62,6 +62,7 @@ class RulesTests: XCTestCase {
             + (rules.first?.name == "organizeDeclarations" ? [] : ["organizeDeclarations"])
             + (rules.first?.name == "extensionAccessControl" ? [] : ["extensionAccessControl"])
             + (rules.first?.name == "markTypes" ? [] : ["markTypes"])
+            + (rules.first?.name == "blockToLineComments" ? [] : ["blockToLineComments"])
         XCTAssertEqual(try format(input, rules: rules, options: options), output, file: file, line: line)
         XCTAssertEqual(try format(input, rules: FormatRules.all(except: exclude), options: options),
                        output2, file: file, line: line)

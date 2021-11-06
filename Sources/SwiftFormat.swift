c6ef3d0700b32f6512feb31a4a9fbc8122433af9
Nick Lockwood <nick@charcoaldesign.co.uk>
2021-11-06 20:38:06 +0000

    Update for 0.49.0 release

--- Sources/SwiftFormat.swift
+++ Sources/SwiftFormat.swift
@@ -32,7 +32,7 @@
 import Foundation
 
 /// The current SwiftFormat version
-let swiftFormatVersion = "0.48.18"
+let swiftFormatVersion = "0.49.0"
 public let version = swiftFormatVersion
 
 /// The standard SwiftFormat config file name

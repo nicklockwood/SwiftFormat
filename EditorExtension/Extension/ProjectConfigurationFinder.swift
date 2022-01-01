fe5a43c681f43c2441dd322d86b134152ab7ce16
Shangxin Guo <int123c@gmail.com>
2022-01-01 19:01:25 +0800

    Add project configuration finder to Xcode extension

---
+++ EditorExtension/Extension/ProjectConfigurationFinder.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectConfigurationFinder.swift
+//  Editor Extension
+//
+//  Created by Shangxin Guo on 2022/1/1.
+//  Copyright © 2022 Nick Lockwood. All rights reserved.
+//
+
+import Foundation
+
+private let connection: NSXPCConnection = {
+    let connection = NSXPCConnection(
+        serviceName: "com.charcoaldesign.SwiftFormat-for-Xcode.EditorExtensionXPCService"
+    )
+    connection.remoteObjectInterface =
+        NSXPCInterface(with: ConfigurationFinderServiceProtocol.self)
+    connection.resume()
+    return connection
+}()
+
+struct ProjectConfigurationFinder {
+    func findProjectOptions(onCompletion: @escaping (Options?) -> Void) {
+        let service = connection.remoteObjectProxyWithErrorHandler {
+            print($0)
+        } as! ConfigurationFinderServiceProtocol
+        service.findConfiguration {
+            if let c = $0, let options = try? Options(c, in: "") {
+                return onCompletion(options)
+            }
+
+            return onCompletion(nil)
+        }
+    }
+}

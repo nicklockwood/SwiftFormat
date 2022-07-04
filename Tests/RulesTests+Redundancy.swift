bc23ac585ab9f342e94a1f8297e3a17f1b50ad08
Nick Lockwood <nick@charcoaldesign.co.uk>
2022-07-04 23:57:51 +0100

    Add redundantOptionalBinding rule for Swift 5.7+ (SE-0345) (#1165)

--- Tests/RulesTests+Redundancy.swift
+++ Tests/RulesTests+Redundancy.swift
@@ -5791,4 +5791,177 @@ class RedundancyTests: RulesTests {
         let input = "let foo = try bar ?? { throw NSError() }()"
         testFormatting(for: input, rule: FormatRules.redundantClosure)
     }
+
+    // MARK: Redundant optional
+
+    func testRemovesRedundantOptionalInSwift5_7() {
+        let input = """
+        if let foo = foo {
+            print(foo)
+        }
+
+        else if var bar = bar {
+            print(bar)
+        }
+
+        guard let self = self else {
+            return
+        }
+
+        while var quux = quux {
+            break
+        }
+        """
+
+        let output = """
+        if let foo {
+            print(foo)
+        }
+
+        else if var bar {
+            print(bar)
+        }
+
+        guard let self else {
+            return
+        }
+
+        while var quux {
+            break
+        }
+        """
+
+        let options = FormatOptions(swiftVersion: "5.7")
+        testFormatting(for: input, output, rule: FormatRules.redundantOptionalBinding, options: options, exclude: ["elseOnSameLine"])
+    }
+
+    func testRemovesMultipleOptionalBindings() {
+        let input = """
+        if let foo = foo, let bar = bar, let baaz = baaz {
+            print(foo, bar, baaz)
+        }
+
+        guard let foo = foo, let bar = bar, let baaz = baaz else {
+            return
+        }
+        """
+
+        let output = """
+        if let foo, let bar, let baaz {
+            print(foo, bar, baaz)
+        }
+
+        guard let foo, let bar, let baaz else {
+            return
+        }
+        """
+
+        let options = FormatOptions(swiftVersion: "5.7")
+        testFormatting(for: input, output, rule: FormatRules.redundantOptionalBinding, options: options)
+    }
+
+    func testRemovesMultipleOptionalBindingsOnSeparateLines() {
+        let input = """
+        if
+          let foo = foo,
+          let bar = bar,
+          let baaz = baaz
+        {
+          print(foo, bar, baaz)
+        }
+
+        guard
+          let foo = foo,
+          let bar = bar,
+          let baaz = baaz
+        else {
+          return
+        }
+        """
+
+        let output = """
+        if
+          let foo,
+          let bar,
+          let baaz
+        {
+          print(foo, bar, baaz)
+        }
+
+        guard
+          let foo,
+          let bar,
+          let baaz
+        else {
+          return
+        }
+        """
+
+        let options = FormatOptions(indent: "  ", swiftVersion: "5.7")
+        testFormatting(for: input, output, rule: FormatRules.redundantOptionalBinding, options: options)
+    }
+
+    func testKeepsRedundantOptionalBeforeSwift5_7() {
+        let input = """
+        if let foo = foo {
+            print(foo)
+        }
+        """
+
+        let options = FormatOptions(swiftVersion: "5.6")
+        testFormatting(for: input, rule: FormatRules.redundantOptionalBinding, options: options)
+    }
+
+    func testKeepsNonRedundantOptional() {
+        let input = """
+        if let foo = bar {
+            print(foo)
+        }
+        """
+
+        let options = FormatOptions(swiftVersion: "5.7")
+        testFormatting(for: input, rule: FormatRules.redundantOptionalBinding, options: options)
+    }
+
+    func testKeepsOptionalNotEligibleForShorthand() {
+        let input = """
+        if let foo = self.foo, let bar = bar(), let baaz = baaz[0] {
+            print(foo, bar, baaz)
+        }
+        """
+
+        let options = FormatOptions(swiftVersion: "5.7")
+        testFormatting(for: input, rule: FormatRules.redundantOptionalBinding, options: options, exclude: ["redundantSelf"])
+    }
+
+    func testRedundantSelfAndRedundantOptionalTogether() {
+        let input = """
+        if let foo = self.foo {
+            print(foo)
+        }
+        """
+
+        let output = """
+        if let foo {
+            print(foo)
+        }
+        """
+
+        let options = FormatOptions(swiftVersion: "5.7")
+        testFormatting(for: input, [output], rules: [FormatRules.redundantOptionalBinding, FormatRules.redundantSelf], options: options)
+    }
+
+    func testDoesntRemoveShadowingOutsideOfOptionalBinding() {
+        let input = """
+        let foo = foo
+
+        if let bar = baaz({
+            let foo = foo
+            print(foo)
+        }) {}
+        """
+
+        let options = FormatOptions(swiftVersion: "5.7")
+        testFormatting(for: input, rule: FormatRules.redundantOptionalBinding, options: options)
+    }
 }

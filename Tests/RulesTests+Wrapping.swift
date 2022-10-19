bff7d2f2dce3ffda79e374569406af376038ed5e
Nick Lockwood <nick@charcoaldesign.co.uk>
2022-10-19 12:47:09 +0300

    Fix bug and crash in wrapComments rule

--- Tests/RulesTests+Wrapping.swift
+++ Tests/RulesTests+Wrapping.swift
@@ -3969,6 +3969,43 @@ class WrappingTests: RulesTests {
                        options: FormatOptions(maxWidth: 6))
     }
 
+    func testWrapSingleLineCommentThatOverflowsByOneCharacter() {
+        let input = """
+        // a b cde fg h
+        """
+        let output = """
+        // a b cde fg
+        // h
+        """
+
+        testFormatting(for: input, output, rule: FormatRules.wrapSingleLineComments,
+                       options: FormatOptions(maxWidth: 14))
+    }
+
+    func testNoWrapSingleLineCommentThatExactlyFits() {
+        let input = """
+        // a b cde fg h
+        """
+
+        testFormatting(for: input, rule: FormatRules.wrapSingleLineComments,
+                       options: FormatOptions(maxWidth: 15))
+    }
+
+    func testWrapSingleLineCommentWithNoLeadingSpace() {
+        let input = """
+        //a b cde fgh
+        """
+        let output = """
+        //a b
+        //cde
+        //fgh
+        """
+
+        testFormatting(for: input, output, rule: FormatRules.wrapSingleLineComments,
+                       options: FormatOptions(maxWidth: 6),
+                       exclude: ["spaceInsideComments"])
+    }
+
     func testWrapDocComment() {
         let input = """
         /// a b cde fgh
@@ -3981,6 +4018,21 @@ class WrappingTests: RulesTests {
 
         testFormatting(for: input, output, rule: FormatRules.wrapSingleLineComments,
                        options: FormatOptions(maxWidth: 7))
+    }
+
+    func testWrapDocLineCommentWithNoLeadingSpace() {
+        let input = """
+        ///a b cde fgh
+        """
+        let output = """
+        ///a b
+        ///cde
+        ///fgh
+        """
+
+        testFormatting(for: input, output, rule: FormatRules.wrapSingleLineComments,
+                       options: FormatOptions(maxWidth: 6),
+                       exclude: ["spaceInsideComments"])
     }
 
     func testWrapSingleLineCommentWithIndent() {
@@ -4018,5 +4070,32 @@ class WrappingTests: RulesTests {
 
         testFormatting(for: input, output, rule: FormatRules.wrapSingleLineComments,
                        options: FormatOptions(maxWidth: 29), exclude: ["wrap"])
+    }
+
+    func testWrapDocCommentWithLongURL() {
+        let input = """
+        /// See [Link](https://www.domain.com/pathextension/pathextension/pathextension/pathextension/pathextension/pathextension).
+        """
+        let output = """
+        /// See
+        /// [Link](https://www.domain.com/pathextension/pathextension/pathextension/pathextension/pathextension/pathextension).
+        """
+
+        testFormatting(for: input, output, rule: FormatRules.wrapSingleLineComments,
+                       options: FormatOptions(maxWidth: 100))
+    }
+
+    func testWrapDocCommentWithLongURL2() {
+        let input = """
+        /// Link to SDK documentation - https://docs.adyen.com/checkout/3d-secure/native-3ds2/api-integration#collect-the-3d-secure-2-device-fingerprint-from-an-ios-app
+        """
+
+        let output = """
+        /// Link to SDK documentation -
+        /// https://docs.adyen.com/checkout/3d-secure/native-3ds2/api-integration#collect-the-3d-secure-2-device-fingerprint-from-an-ios-app
+        """
+
+        testFormatting(for: input, output, rule: FormatRules.wrapSingleLineComments,
+                       options: FormatOptions(maxWidth: 80))
     }
 }

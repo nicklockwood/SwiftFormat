da637c398c5d08896521b737f2868ddc2e7996ae
Nick Lockwood <nick@charcoaldesign.co.uk>
2022-12-03 21:45:21 +0000

    Update for 0.50.6 release

--- Sources/SwiftFormat.swift
+++ Sources/SwiftFormat.swift
@@ -32,7 +32,7 @@
 import Foundation
 
 /// The current SwiftFormat version
-let swiftFormatVersion = "0.50.5"
+let swiftFormatVersion = "0.50.6"
 public let version = swiftFormatVersion
 
 /// The standard SwiftFormat config file name

68188d49ec636c1dbabb29c8fea0d2eac2c578c8
Nick Lockwood <nick@charcoaldesign.co.uk>
2023-02-09 21:57:58 +0000

    Make extension instructions dynamic based on OS version

--- Tests/SwiftFormatTests.swift
+++ Tests/SwiftFormatTests.swift
@@ -67,7 +67,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 67)
+        XCTAssertEqual(files.count, 68)
     }
 
     func testInputFilesMatchOutputFilesForSameOutput() {
@@ -78,7 +78,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 67)
+        XCTAssertEqual(files.count, 68)
     }
 
     func testInputFileNotEnumeratedWhenExcluded() {
@@ -93,7 +93,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 44)
+        XCTAssertEqual(files.count, 45)
     }
 
     // MARK: format function

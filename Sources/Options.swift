f61d1cc1647c8368739b93e1b9674f7821dc373c
Cal Stephens <cal@calstephens.tech>
2023-05-14 08:22:18 -0700

    Remove `--elseblankline`, make it the default behavior

--- Sources/Options.swift
+++ Sources/Options.swift
@@ -360,14 +360,6 @@ public enum SpaceAroundDelimiter: String, CaseIterable {
     case leadingTrailing = "leading-trailing"
 }
 
-/// Whether or not to reformat an `else` statement preceded by a blank line
-public enum BlankLineBeforeElse: String, CaseIterable {
-    /// Always update the `else` statement using the `elseOnSameLine` rule
-    case update
-    /// If there is a blank line before the `else` statement, don't apply the `elseOnSameLine` rule
-    case preserve
-}
-
 /// Configuration options for formatting. These aren't actually used by the
 /// Formatter class itself, but it makes them available to the format rules.
 public struct FormatOptions: CustomStringConvertible {
@@ -408,7 +400,6 @@ public struct FormatOptions: CustomStringConvertible {
     public var stripUnusedArguments: ArgumentStrippingMode
     public var elseOnNextLine: Bool
     public var guardElsePosition: ElsePosition
-    public var blankLineBeforeElse: BlankLineBeforeElse
     public var explicitSelf: SelfMode
     public var selfRequired: Set<String>
     public var throwCapturing: Set<String>
@@ -510,7 +501,6 @@ public struct FormatOptions: CustomStringConvertible {
                 stripUnusedArguments: ArgumentStrippingMode = .all,
                 elseOnNextLine: Bool = false,
                 guardElsePosition: ElsePosition = .auto,
-                blankLineBeforeElse: BlankLineBeforeElse = .update,
                 explicitSelf: SelfMode = .remove,
                 selfRequired: Set<String> = [],
                 throwCapturing: Set<String> = [],
@@ -603,7 +593,6 @@ public struct FormatOptions: CustomStringConvertible {
         self.stripUnusedArguments = stripUnusedArguments
         self.elseOnNextLine = elseOnNextLine
         self.guardElsePosition = guardElsePosition
-        self.blankLineBeforeElse = blankLineBeforeElse
         self.explicitSelf = explicitSelf
         self.selfRequired = selfRequired
         self.throwCapturing = throwCapturing

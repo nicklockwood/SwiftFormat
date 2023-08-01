12916979c0b5c59995c16b2e8513413756221619
Cal Stephens <cal@calstephens.tech>
2023-08-01 15:07:27 -0700

    Add rule to convert `forEach { ... }` calls to for loops (#1490)

--- Tests/SwiftFormatTests.swift
+++ Tests/SwiftFormatTests.swift
@@ -67,7 +67,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 67)
+        XCTAssertEqual(files.count, 68)
     }
 
     func testInputFilesMatchOutputFilesForSameOutput() {
@@ -78,7 +78,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 67)
+        XCTAssertEqual(files.count, 68)
     }
 
     func testInputFileNotEnumeratedWhenExcluded() {
@@ -93,7 +93,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 42)
+        XCTAssertEqual(files.count, 43)
     }
 
     // MARK: format function

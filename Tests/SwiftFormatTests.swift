46f765637d7fd1ac4658dea65c1ee02411bc416e
Hampus Tågerud <hampus.tagerud@gmail.com>
2023-08-09 00:09:07 +0200

    Add `created.name` and `created.email` file header placeholders

--- Tests/SwiftFormatTests.swift
+++ Tests/SwiftFormatTests.swift
@@ -67,7 +67,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 69)
+        XCTAssertEqual(files.count, 71)
     }
 
     func testInputFilesMatchOutputFilesForSameOutput() {
@@ -78,7 +78,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 69)
+        XCTAssertEqual(files.count, 71)
     }
 
     func testInputFileNotEnumeratedWhenExcluded() {
@@ -93,7 +93,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 43)
+        XCTAssertEqual(files.count, 45)
     }
 
     // MARK: format function

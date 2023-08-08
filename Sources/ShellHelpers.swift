973071209d451337efd91a286879b3578861e020
Hampus Tågerud <hampus.tagerud@gmail.com>
2023-08-09 00:09:07 +0200

    Add `created.name` and `created.email` file header placeholders

---
+++ Sources/ShellHelpers.swift
@@ -0,0 +1,59 @@
+//
+//  ShellHelpers.swift
+//  SwiftFormat
+//
+//  Created by Hampus Tågerud on 2023-08-08.
+//  Copyright 2023 Nick Lockwood and the SwiftFormat project authors
+//
+//  Distributed under the permissive MIT license
+//  Get the latest version from here:
+//
+//  https://github.com/nicklockwood/SwiftFormat
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+extension String {
+    func shellOutput() -> String? {
+        let process = Process()
+        let pipe = Pipe()
+
+        process.executableURL = URL(string: "/bin/bash")
+        process.arguments = ["-c", self]
+        process.standardOutput = pipe
+        process.standardError = pipe
+
+        let file = pipe.fileHandleForReading
+
+        do { try process.run() }
+        catch { return nil }
+
+        process.waitUntilExit()
+
+        guard process.terminationStatus == 0 else {
+            return nil
+        }
+
+        let outputData = file.readDataToEndOfFile()
+        return String(data: outputData, encoding: .utf8)?
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}

78334b466fd960d166b574efcab14baa2ee4c385
Hampus Tågerud <hampus.tagerud@gmail.com>
2023-08-16 22:40:32 +0200

    Reduce number of shell commands to get git info and improve reliability

--- Sources/GitHelpers.swift
+++ Sources/GitHelpers.swift
@@ -31,89 +31,127 @@
 
 import Foundation
 
+private func memoize<K, T>(_ keyFn: @escaping (K) -> String?,
+                           _ workFn: @escaping (K) -> T) -> (K) -> T
+{
+    let lock = NSLock()
+    var cache: [String: T] = [:]
+
+    return { input in
+        let key = keyFn(input) ?? "@nil"
+
+        lock.lock()
+        defer { lock.unlock() }
+
+        if let value = cache[key] {
+            return value
+        }
+
+        let newValue = workFn(input)
+        cache[key] = newValue
+
+        return newValue
+    }
+}
+
 struct GitFileInfo {
     var createdByName: String?
     var createdByEmail: String?
     var createdAt: Date?
 }
 
-struct GitHelpers {
-    var currentWorkingDirectory: URL?
+enum GitHelpers {
+    static let getGitRoot: (URL) -> URL? = memoize({ $0.relativePath }) { url in
+        let dir = "git rev-parse --show-toplevel".shellOutput(cwd: url)
 
-    init(cwd: URL?) {
-        currentWorkingDirectory = cwd
+        guard let root = dir, FileManager.default.fileExists(atPath: root) else {
+            return nil
+        }
+
+        return URL(fileURLWithPath: root, isDirectory: true)
     }
 
-    private var inGitRoot: Bool {
-        // Get current git repository top level directory
-        guard let root = "git rev-parse --show-toplevel"
-            .shellOutput(cwd: currentWorkingDirectory) else { return false }
-        // Make sure a valid URL was returned
-        guard let _ = URL(string: root) else { return false }
-        // Make sure an existing path was returned
-        return FileManager.default.fileExists(atPath: root)
-    }
-
-    // If a file has never been committed, defaults to the local git user for that repository
-    private var defaultGitInfo: GitFileInfo? {
-        guard inGitRoot else { return nil }
-
-        let name = "git config user.name"
-            .shellOutput(cwd: currentWorkingDirectory)
-        let email = "git config user.email"
-            .shellOutput(cwd: currentWorkingDirectory)
+    // If a file has never been committed, default to the local git user for the repository
+    static let getDefaultGitInfo: (URL) -> GitFileInfo? = memoize({ $0.relativePath }) { url in
+        let name = "git config user.name".shellOutput(cwd: url)
+        let email = "git config user.email".shellOutput(cwd: url)
 
         guard let safeName = name, let safeEmail = email else { return nil }
 
         return GitFileInfo(createdByName: safeName, createdByEmail: safeEmail)
     }
 
-    private enum FileInfoPart: String {
-        case email = "ae"
-        case name = "an"
-        case createdAt = "at"
-    }
+    private static func getGitCommit(_ url: URL, root: URL, follow: Bool) -> String? {
+        let command = [
+            "git log",
+            // --follow to keep tracking the file across renames
+            follow ? "--follow" : "",
+            "--diff-filter=A",
+            "--author-date-order",
+            "--pretty=%H",
+            url.relativePath,
+        ]
+        .filter { ($0?.count ?? 0) > 0 }
+        .joined(separator: " ")
 
-    private func fileInfoPart(_ inputURL: URL,
-                              _ part: FileInfoPart,
-                              follow: Bool) -> String?
-    {
-        // --follow to keep tracking the file across renames
-        let follow = follow ? "--follow" : ""
-        let format = part.rawValue
-        let path = inputURL.relativePath
+        let output = command.shellOutput(cwd: root)
 
-        let value = "git log \(follow) --diff-filter=A --pretty=%\(format) \(path)"
-            .shellOutput(cwd: currentWorkingDirectory)
+        guard let safeValue = output, !safeValue.isEmpty else { return nil }
 
-        guard let safeValue = value, !safeValue.isEmpty else { return nil }
+        if safeValue.contains("\n") {
+            let parts = safeValue.split(separator: "\n")
+
+            if parts.count > 1, let first = parts.first {
+                return String(first)
+            }
+        }
+
         return safeValue
     }
 
-    func fileInfo(_ inputURL: URL, follow: Bool) -> GitFileInfo? {
-        guard inGitRoot else { return nil }
+    static var json: JSONDecoder { JSONDecoder() }
 
-        let name = fileInfoPart(inputURL, .name, follow: follow) ??
-            defaultGitInfo?.createdByName
-        let email = fileInfoPart(inputURL, .email, follow: follow) ??
-            defaultGitInfo?.createdByEmail
+    static let getCommitInfo: ((String, URL)) -> GitFileInfo? = memoize(
+        { hash, root in hash + root.relativePath },
+        { hash, root in
+            let format = #"{"name":"%an","email":"%ae","time":"%at"}"#
+            let command = "git show --format='\(format)' -s \(hash)"
+            guard let commitInfo = command.shellOutput(cwd: root) else {
+                return nil
+            }
 
-        var date: Date?
-        if let createdAtString = fileInfoPart(inputURL, .createdAt, follow: follow),
-           let interval = TimeInterval(createdAtString)
-        {
-            date = Date(timeIntervalSince1970: interval)
+            guard let commitData = commitInfo.data(using: .utf8) else {
+                return nil
+            }
+
+            let MapType = [String: String].self
+            guard let dict = try? json.decode(MapType, from: commitData) else {
+                return nil
+            }
+
+            let (name, email) = (dict["name"], dict["email"])
+
+            var date: Date?
+            if let createdAtString = dict["time"],
+               let interval = TimeInterval(createdAtString)
+            {
+                date = Date(timeIntervalSince1970: interval)
+            }
+
+            return GitFileInfo(createdByName: name,
+                               createdByEmail: email,
+                               createdAt: date)
+        }
+    )
+
+    static func fileInfo(_ url: URL, follow: Bool = false) -> GitFileInfo? {
+        let dir = url.deletingLastPathComponent()
+        guard let gitRoot = getGitRoot(dir) else { return nil }
+
+        guard let commitHash = getGitCommit(url, root: gitRoot, follow: follow) else {
+            return nil
         }
 
-        return GitFileInfo(createdByName: name,
-                           createdByEmail: email,
-                           createdAt: date)
-    }
-
-    static func fileInfo(_ inputURL: URL,
-                         cwd: URL? = nil,
-                         follow: Bool = false) -> GitFileInfo?
-    {
-        GitHelpers(cwd: cwd).fileInfo(inputURL, follow: follow)
+        return getCommitInfo((commitHash, gitRoot))
     }
 }

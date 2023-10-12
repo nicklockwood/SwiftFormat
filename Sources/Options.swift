26eadcbefe4c56b4fb06f61891cc727915c79498
Robert Gering <mail@robertgering.de>
2023-10-12 15:31:10 +0200

    Add option for forcing closing paren on same line of function calls

--- Sources/Options.swift
+++ Sources/Options.swift
@@ -594,6 +594,7 @@ public struct FormatOptions: CustomStringConvertible {
     public var wrapTypealiases: WrapMode
     public var wrapEnumCases: WrapEnumCases
     public var closingParenOnSameLine: Bool
+    public var forceClosingParenOnSameLineForFunctionCalls: Bool
     public var wrapReturnType: WrapReturnType
     public var wrapConditions: WrapMode
     public var wrapTernaryOperators: TernaryOperatorWrapMode
@@ -701,6 +702,7 @@ public struct FormatOptions: CustomStringConvertible {
                 wrapTypealiases: WrapMode = .preserve,
                 wrapEnumCases: WrapEnumCases = .always,
                 closingParenOnSameLine: Bool = false,
+                forceClosingParenOnSameLineForFunctionCalls: Bool = false,
                 wrapReturnType: WrapReturnType = .preserve,
                 wrapConditions: WrapMode = .preserve,
                 wrapTernaryOperators: TernaryOperatorWrapMode = .default,
@@ -798,6 +800,7 @@ public struct FormatOptions: CustomStringConvertible {
         self.wrapTypealiases = wrapTypealiases
         self.wrapEnumCases = wrapEnumCases
         self.closingParenOnSameLine = closingParenOnSameLine
+        self.forceClosingParenOnSameLineForFunctionCalls = forceClosingParenOnSameLineForFunctionCalls
         self.wrapReturnType = wrapReturnType
         self.wrapConditions = wrapConditions
         self.wrapTernaryOperators = wrapTernaryOperators

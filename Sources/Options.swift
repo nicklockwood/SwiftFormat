7d27f9aed7dc906da4cc2eb4eab8f17d75189a73
Nick Lockwood <nick@charcoaldesign.co.uk>
2023-10-14 09:17:38 +0100

    Rename `closingparenfcall` to `callsiteparen`

--- Sources/Options.swift
+++ Sources/Options.swift
@@ -594,7 +594,7 @@ public struct FormatOptions: CustomStringConvertible {
     public var wrapTypealiases: WrapMode
     public var wrapEnumCases: WrapEnumCases
     public var closingParenOnSameLine: Bool
-    public var forceClosingParenOnSameLineForFunctionCalls: Bool
+    public var closingCallSiteParenOnSameLine: Bool
     public var wrapReturnType: WrapReturnType
     public var wrapConditions: WrapMode
     public var wrapTernaryOperators: TernaryOperatorWrapMode
@@ -702,7 +702,7 @@ public struct FormatOptions: CustomStringConvertible {
                 wrapTypealiases: WrapMode = .preserve,
                 wrapEnumCases: WrapEnumCases = .always,
                 closingParenOnSameLine: Bool = false,
-                forceClosingParenOnSameLineForFunctionCalls: Bool = false,
+                closingCallSiteParenOnSameLine: Bool = false,
                 wrapReturnType: WrapReturnType = .preserve,
                 wrapConditions: WrapMode = .preserve,
                 wrapTernaryOperators: TernaryOperatorWrapMode = .default,
@@ -800,7 +800,7 @@ public struct FormatOptions: CustomStringConvertible {
         self.wrapTypealiases = wrapTypealiases
         self.wrapEnumCases = wrapEnumCases
         self.closingParenOnSameLine = closingParenOnSameLine
-        self.forceClosingParenOnSameLineForFunctionCalls = forceClosingParenOnSameLineForFunctionCalls
+        self.closingCallSiteParenOnSameLine = closingCallSiteParenOnSameLine
         self.wrapReturnType = wrapReturnType
         self.wrapConditions = wrapConditions
         self.wrapTernaryOperators = wrapTernaryOperators

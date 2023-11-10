a2184d502710ddf34b7632b8c98549f5ebb5fcb6
MahdiBM <github@mahdibm.com>
2023-11-10 09:31:54 +0330

    fix tests

--- Tests/SwiftFormatTests.swift
+++ Tests/SwiftFormatTests.swift
@@ -93,7 +93,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 42)
+        XCTAssertEqual(files.count, 41)
     }
 
     // MARK: format function

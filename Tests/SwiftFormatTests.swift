a538465f1fa3051a26e7ffdebf0a3d88498bab8a
MahdiBM <github@mahdibm.com>
2023-11-10 23:01:20 +0330

    more tests fixes

--- Tests/SwiftFormatTests.swift
+++ Tests/SwiftFormatTests.swift
@@ -67,7 +67,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 67)
+        XCTAssertEqual(files.count, 66)
     }
 
     func testInputFilesMatchOutputFilesForSameOutput() {
@@ -78,7 +78,7 @@ class SwiftFormatTests: XCTestCase {
             return { files.append(inputURL) }
         }
         XCTAssertEqual(errors.count, 0)
-        XCTAssertEqual(files.count, 67)
+        XCTAssertEqual(files.count, 66)
     }
 
     func testInputFileNotEnumeratedWhenExcluded() {

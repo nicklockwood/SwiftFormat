2915b97072284d8a0ccdd4c528db9d484727dd30
Cal Stephens <cal.stephens@airbnb.com>
2023-11-20 17:18:38 -0800

    Support configuring wrapAttributes rule to apply differently to computed properties vs stored properties

--- Sources/Options.swift
+++ Sources/Options.swift
@@ -602,6 +602,7 @@ public struct FormatOptions: CustomStringConvertible {
     public var funcAttributes: AttributeMode
     public var typeAttributes: AttributeMode
     public var varAttributes: AttributeMode
+    public var storedVarAttributes: AttributeMode
     public var markTypes: MarkMode
     public var typeMarkComment: String
     public var markExtensions: MarkMode
@@ -710,6 +711,7 @@ public struct FormatOptions: CustomStringConvertible {
                 funcAttributes: AttributeMode = .preserve,
                 typeAttributes: AttributeMode = .preserve,
                 varAttributes: AttributeMode = .preserve,
+                storedVarAttributes: AttributeMode = .preserve,
                 markTypes: MarkMode = .always,
                 typeMarkComment: String = "MARK: - %t",
                 markExtensions: MarkMode = .always,
@@ -808,6 +810,7 @@ public struct FormatOptions: CustomStringConvertible {
         self.funcAttributes = funcAttributes
         self.typeAttributes = typeAttributes
         self.varAttributes = varAttributes
+        self.storedVarAttributes = storedVarAttributes
         self.markTypes = markTypes
         self.typeMarkComment = typeMarkComment
         self.markExtensions = markExtensions

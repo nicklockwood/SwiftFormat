c1d4df5498d62e7e3f88223bd5cb347ac3b88eea
Cal Stephens <cal@calstephens.tech>
2023-11-28 19:17:12 -0800

    Include @Environment as non-complex attribute by default

--- Tests/RulesTests+Wrapping.swift
+++ Tests/RulesTests+Wrapping.swift
@@ -4891,7 +4891,7 @@ class WrappingTests: RulesTests {
             }
         }
         """
-        let options = FormatOptions(varAttributes: .sameLine, storedVarAttributes: .sameLine, computedVarAttributes: .prevLine, complexAttributes: .prevLine, complexAttributesExceptions: ["@Environment"])
+        let options = FormatOptions(varAttributes: .sameLine, storedVarAttributes: .sameLine, computedVarAttributes: .prevLine, complexAttributes: .prevLine)
         testFormatting(for: input, output, rule: FormatRules.wrapAttributes, options: options)
     }
 
@@ -4931,7 +4931,7 @@ class WrappingTests: RulesTests {
         }
         """
 
-        let options = FormatOptions(varAttributes: .sameLine, complexAttributes: .prevLine, complexAttributesExceptions: ["@Environment"])
+        let options = FormatOptions(varAttributes: .sameLine, complexAttributes: .prevLine)
         testFormatting(for: input, rule: FormatRules.wrapAttributes, options: options)
     }
 

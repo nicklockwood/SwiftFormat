f7a8c91421ac282c4b186f2077567ba663058e33
Cal Stephens <cal.stephens@airbnb.com>
2024-03-18 15:18:25 -0700

    Add --condassignment after-property option

--- Sources/Examples.swift
+++ Sources/Examples.swift
@@ -1683,6 +1683,7 @@ private struct Examples {
       }
     ```
 
+    // With --condassignment always (disabled by default)
     - switch condition {
     + foo.bar = switch condition {
       case true:
--- Sources/Options.swift
+++ Sources/Options.swift
@@ -664,6 +664,7 @@ public struct FormatOptions: CustomStringConvertible {
     public var preserveAnonymousForEach: Bool
     public var preserveSingleLineForEach: Bool
     public var preserveDocComments: Bool
+    public var conditionalAssignmentOnlyAfterNewProperties: Bool
     public var spaceAroundDelimiter: SpaceAroundDelimiter
     public var initCoderNil: Bool
     public var dateFormat: DateFormat
@@ -777,6 +778,7 @@ public struct FormatOptions: CustomStringConvertible {
                 preserveAnonymousForEach: Bool = false,
                 preserveSingleLineForEach: Bool = true,
                 preserveDocComments: Bool = false,
+                conditionalAssignmentOnlyAfterNewProperties: Bool = true,
                 spaceAroundDelimiter: SpaceAroundDelimiter = .trailing,
                 initCoderNil: Bool = false,
                 dateFormat: DateFormat = .system,
@@ -880,6 +882,7 @@ public struct FormatOptions: CustomStringConvertible {
         self.preserveAnonymousForEach = preserveAnonymousForEach
         self.preserveSingleLineForEach = preserveSingleLineForEach
         self.preserveDocComments = preserveDocComments
+        self.conditionalAssignmentOnlyAfterNewProperties = conditionalAssignmentOnlyAfterNewProperties
         self.spaceAroundDelimiter = spaceAroundDelimiter
         self.initCoderNil = initCoderNil
         self.dateFormat = dateFormat

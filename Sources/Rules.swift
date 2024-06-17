4433c16db421a0ab6e22fe8edbf677367b8a0f1f
Rakuyo <rakuyo.mo@gmail.com>
2024-06-17 21:56:02 +0800

    Fix `--nilInit insert` being applied to computed property (#1735)

--- Sources/Rules.swift
+++ Sources/Rules.swift
@@ -2951,7 +2951,7 @@ public struct _FormatRules {
         help: "Remove/insert redundant `nil` default value (Optional vars are nil by default).",
         options: ["nilinit"]
     ) { formatter in
-        func search(from index: Int) {
+        func search(from index: Int, isStoredProperty: Bool) {
             if let optionalIndex = formatter.index(of: .unwrapOperator, after: index) {
                 if formatter.index(of: .endOfStatement, in: index + 1 ..< optionalIndex) != nil {
                     return
@@ -2968,13 +2968,13 @@ public struct _FormatRules {
                             formatter.removeTokens(in: optionalIndex + 1 ... nilIndex)
                         }
                     case .insert:
-                        if equalsIndex == nil {
+                        if isStoredProperty && equalsIndex == nil {
                             let tokens: [Token] = [.space(" "), .operator("=", .infix), .space(" "), .identifier("nil")]
                             formatter.insert(tokens, at: optionalIndex + 1)
                         }
                     }
                 }
-                search(from: optionalIndex)
+                search(from: optionalIndex, isStoredProperty: isStoredProperty)
             }
         }
 
@@ -3006,7 +3006,7 @@ public struct _FormatRules {
                 }
             }
             // Find the nil
-            search(from: i)
+            search(from: i, isStoredProperty: formatter.isStoredProperty(atIntroducerIndex: i))
         }
     }
 

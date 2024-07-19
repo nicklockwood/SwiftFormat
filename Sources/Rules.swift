a6a5e2d110cedcd144206801cf6986879b8b42ca
oiuhr <oiuhr@yandex.ru>
2024-07-20 01:13:25 +0300

    `organizeDeclarations` custom naming & ordering (#1736)

--- Sources/Rules.swift
+++ Sources/Rules.swift
@@ -5706,6 +5706,7 @@ public struct _FormatRules {
             "categorymark", "markcategories", "beforemarks",
             "lifecycle", "organizetypes", "structthreshold", "classthreshold",
             "enumthreshold", "extensionlength", "organizationmode",
+            "visibilityorder", "typeorder", "visibilitymarks", "typemarks",
         ],
         sharedOptions: ["sortedpatterns", "lineaftermarks"]
     ) { formatter in

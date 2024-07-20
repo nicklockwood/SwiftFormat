51b369af8d77c89ee4379502d1091c118b85409e
Manny Lopez <manuel.est.lopez@gmail.com>
2024-07-19 21:52:01 -0700

    Add allowList to unusedPrivateDeclaration (#1761)

--- Tests/RulesTests+Redundancy.swift
+++ Tests/RulesTests+Redundancy.swift
@@ -10617,4 +10617,14 @@ class RedundancyTests: RulesTests {
 
         testFormatting(for: input, output, rule: FormatRules.unusedPrivateDeclaration)
     }
+
+    func testDoNotRemovePrivateTypealias() {
+        let input = """
+        enum Foo {
+            struct Bar {}
+            private typealias Baz = Bar
+        }
+        """
+        testFormatting(for: input, rule: FormatRules.unusedPrivateDeclaration)
+    }
 }

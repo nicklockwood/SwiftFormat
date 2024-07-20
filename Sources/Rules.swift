cde89e44bc9a2efe6598adaf84ba68fa3ef82ffb
Manny Lopez <manuel.est.lopez@gmail.com>
2024-07-19 21:52:01 -0700

    Add allowList to unusedPrivateDeclaration (#1761)

--- Sources/Rules.swift
+++ Sources/Rules.swift
@@ -5202,6 +5202,7 @@ public struct _FormatRules {
         disabledByDefault: true
     ) { formatter in
         guard !formatter.options.fragment else { return }
+        let allowList = ["let", "var", "func"]
         var privateDeclarations: [Formatter.Declaration] = []
         var usage: [String: Int] = [:]
 
@@ -5224,7 +5225,8 @@ public struct _FormatRules {
                count < 2
             {
                 switch declaration {
-                case let .declaration(_, _, originalRange):
+                case let .declaration(kind, _, originalRange):
+                    guard allowList.contains(kind) else { break }
                     formatter.removeTokens(in: originalRange)
                 case .type, .conditionalCompilation:
                     break

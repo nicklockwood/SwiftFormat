9e4c206ec0f2652f4880d23080888731520c016a
Manny Lopez <manuel.est.lopez@gmail.com>
2024-07-30 14:25:05 -0700

    Add emptyExtension rule + tests (#1792)

---
+++ Sources/Rules/EmptyExtension.swift
@@ -0,0 +1,44 @@
+//
+//  EmptyExtension.swift
+//  SwiftFormat
+//
+// Created by manny_lopez on 7/29/24.
+//  Copyright © 2024 Nick Lockwood. All rights reserved.
+//
+
+import Foundation
+
+public extension FormatRule {
+    /// Remove empty, non-conforming, extensions.
+    static let emptyExtension = FormatRule(
+        help: "Remove empty, non-conforming, extensions.",
+        orderAfter: [.unusedPrivateDeclaration]
+    ) { formatter in
+        var emptyExtensions = [Declaration]()
+
+        formatter.forEachRecursiveDeclaration { declaration in
+            let declarationModifiers = Set(declaration.modifiers)
+            guard declaration.keyword == "extension",
+                  let declarationBody = declaration.body,
+                  declarationBody.isEmpty,
+                  // Ensure that it is not a macro
+                  !declarationModifiers.contains(where: { $0.first == "@" })
+            else { return }
+
+            // Ensure that the extension does not conform to any protocols
+            let parser = Formatter(declaration.openTokens)
+            guard let extensionIndex = parser.index(of: .keyword("extension"), after: -1),
+                  let typeNameIndex = parser.index(of: .nonSpaceOrLinebreak, after: extensionIndex),
+                  let type = parser.parseType(at: typeNameIndex),
+                  let indexAfterType = parser.index(of: .nonSpaceOrCommentOrLinebreak, after: type.range.upperBound),
+                  parser.tokens[indexAfterType] != .delimiter(":")
+            else { return }
+
+            emptyExtensions.append(declaration)
+        }
+
+        for declaration in emptyExtensions.reversed() {
+            formatter.removeTokens(in: declaration.originalRange)
+        }
+    }
+}
---
+++ Tests/Rules/EmptyExtensionTests.swift
@@ -0,0 +1,80 @@
+//
+//  EmptyExtensionTests.swift
+//  SwiftFormatTests
+//
+//  Created by manny_lopez on 7/28/2024.
+//  Copyright © 2024 Nick Lockwood. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftFormat
+
+class EmptyExtensionTests: XCTestCase {
+    func testRemoveEmptyExtension() {
+        let input = """
+        extension String {}
+
+        extension String: Equatable {}
+        """
+        let output = """
+        extension String: Equatable {}
+        """
+        testFormatting(for: input, output, rule: .emptyExtension)
+    }
+
+    func testRemoveNonConformingEmptyExtension() {
+        let input = """
+        extension [Foo: Bar] {}
+
+        extension Array where Element: Foo {}
+        """
+        let output = ""
+        testFormatting(for: input, output, rule: .emptyExtension)
+    }
+
+    func testDoNotRemoveEmptyConformingExtension() {
+        let input = """
+        extension String: Equatable {}
+        """
+        testFormatting(for: input, rule: .emptyExtension)
+    }
+
+    func testDoNotRemoveAtModifierEmptyExtension() {
+        let input = """
+        @GenerateBoilerPlate
+        extension Foo {}
+        """
+        testFormatting(for: input, rule: .emptyExtension)
+    }
+
+    func testRemoveEmptyExtensionWithEmptyBody() {
+        let input = """
+        extension Foo { }
+
+        extension Foo {
+
+        }
+        """
+        let output = ""
+        testFormatting(for: input, output, rule: .emptyExtension)
+    }
+
+    func testRemoveUnusedPrivateDeclarationThenEmptyExtension() {
+        let input = """
+        class Foo {
+            init() {}
+        }
+        extension Foo {
+            private var bar: Bar { "bar" }
+        }
+        """
+
+        let output = """
+        class Foo {
+            init() {}
+        }
+
+        """
+        testFormatting(for: input, [output], rules: [.unusedPrivateDeclaration, .emptyExtension])
+    }
+}

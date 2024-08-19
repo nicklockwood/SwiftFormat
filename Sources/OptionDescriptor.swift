b746c06b982fa6496d9746a5819fa261279c3532
Miguel Jimenez <miguel.jimenez@airbnb.com>
2024-08-19 17:44:50 -0600

    Update OptionDescriptor.swift

--- Sources/OptionDescriptor.swift
+++ Sources/OptionDescriptor.swift
@@ -1229,7 +1229,7 @@ struct _Descriptors {
     let swiftUIPropertiesSortMode = OptionDescriptor(
         argumentName: "sortswiftuiprops",
         displayName: "Sort SwiftUI Dynamic Properties",
-        help: "Sorts SwiftUI dynamic properties, doesn't sorts by default",
+        help: "Sorts SwiftUI dynamic properties, doesn't sort by default",
         keyPath: \.swiftUIPropertiesSortMode
     )
 

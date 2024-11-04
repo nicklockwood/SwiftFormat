e683fb14ed9f1733d9c6aeb0b6d989ae942a233f
Cal Stephens <cal@calstephens.tech>
2024-11-03 18:18:28 -0800

    Make all tests pass

--- Tests/ParsingHelpersTests.swift
+++ Tests/ParsingHelpersTests.swift
@@ -2514,7 +2514,7 @@ class ParsingHelpersTests: XCTestCase {
         XCTAssertFalse(isStoredProperty("""
         var foo: String {
             get { "foo" }
-            set { print(newValue} }
+            set { print(newValue) }
         }
         """))
     }

13e8321fc0081ca677b39e96297e05528e5c6f4b
Nick Lockwood <nick@charcoaldesign.co.uk>
2025-01-20 00:46:45 +0000

    Fix spacingGuards error
    Signed-off-by: Nick Lockwood <nick@charcoaldesign.co.uk>

--- Sources/Rules/SpacingGuards.swift
+++ Sources/Rules/SpacingGuards.swift
@@ -9,28 +9,34 @@ public extension FormatRule {
         disabledByDefault: true
     ) { formatter in
         formatter.forEach(.keyword("guard")) { guardIndex, _ in
-            guard let startOfScopeOfGuard = formatter.index(of: .startOfScope("{"), after: guardIndex),
-                  let endOfScopeOfGuard = formatter.endOfScope(at: startOfScopeOfGuard)
+            guard var elseIndex = formatter.index(of: .keyword("else"), after: guardIndex) else {
+                return
+            }
+            if !formatter.isGuardElse(at: elseIndex) {
+                guard let nextIndex = formatter.index(of: .keyword("else"), after: elseIndex) else {
+                    return
+                }
+                elseIndex = nextIndex
+            }
+            guard let startOfGuardScope = formatter.index(of: .startOfScope("{"), after: elseIndex),
+                  let endOfGuardScope = formatter.endOfScope(at: startOfGuardScope)
             else {
                 return
             }
 
-            guard let nextNonSpaceAndNonLinebreakIndex = formatter.index(of: .nonSpaceOrLinebreak, after: endOfScopeOfGuard) else {
+            guard let nextNonSpaceAndNonLinebreakIndex = formatter.index(of: .nonSpaceOrLinebreak, after: endOfGuardScope) else {
                 return
             }
 
-            let nextNonSpaceAndNonLinebreakToken = formatter.token(at: nextNonSpaceAndNonLinebreakIndex)
-
-            if nextNonSpaceAndNonLinebreakToken == .endOfScope("}")
-                || nextNonSpaceAndNonLinebreakToken?.isOperator == true
-            {
+            let nextToken = formatter.tokens[nextNonSpaceAndNonLinebreakIndex]
+            if nextToken == .endOfScope("}") || nextToken.isOperator {
                 // Do not add space in this cases
                 return
             }
 
-            let isGuard = nextNonSpaceAndNonLinebreakToken == .keyword("guard")
-            let indexesBetween = Set(endOfScopeOfGuard + 1 ..< nextNonSpaceAndNonLinebreakIndex)
-            formatter.leaveOrSetLinebreaksInIndexes(indexesBetween, linebreaksCount: isGuard ? 1 : 2)
+            let linebreaks = nextToken == .keyword("guard") ? 1 : 2
+            let indexesBetween = Set(endOfGuardScope + 1 ..< nextNonSpaceAndNonLinebreakIndex)
+            formatter.leaveOrSetLinebreaksInIndexes(indexesBetween, linebreaksCount: linebreaks)
         }
     } examples: {
         """

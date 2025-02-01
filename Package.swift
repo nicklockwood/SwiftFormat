c538be7ff6f1d98541bd6e603b8f2699dfa20dde
Cal Stephens <cal@calstephens.tech>
2025-02-01 14:24:27 -0800

    Update minimum supported Swift version to Swift 5.7

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
@@ -6,10 +6,31 @@ let package = Package(
     products: [
         .executable(name: "swiftformat", targets: ["CommandLineTool"]),
         .library(name: "SwiftFormat", targets: ["SwiftFormat"]),
+        .plugin(name: "SwiftFormatPlugin", targets: ["SwiftFormatPlugin"]),
     ],
     targets: [
-        .target(name: "CommandLineTool", dependencies: ["SwiftFormat"], path: "CommandLineTool"),
-        .target(name: "SwiftFormat", path: "Sources"),
-        .testTarget(name: "SwiftFormatTests", dependencies: ["SwiftFormat"], path: "Tests"),
+        .executableTarget(
+            name: "CommandLineTool", dependencies: ["SwiftFormat"], path: "CommandLineTool",
+            exclude: ["swiftformat"]
+        ),
+        .target(name: "SwiftFormat", path: "Sources", exclude: ["Info.plist"]),
+        .testTarget(
+            name: "SwiftFormatTests",
+            dependencies: ["SwiftFormat"],
+            path: "Tests",
+            exclude: ["GlobTest[5].txt"]
+        ),
+        .plugin(
+            name: "SwiftFormatPlugin",
+            capability: .command(
+                intent: .custom(
+                    verb: "swiftformat", description: "Formats Swift source files using SwiftFormat"
+                ),
+                permissions: [
+                    .writeToPackageDirectory(reason: "This command reformats source files"),
+                ]
+            ),
+            dependencies: [.target(name: "CommandLineTool")]
+        ),
     ]
 )

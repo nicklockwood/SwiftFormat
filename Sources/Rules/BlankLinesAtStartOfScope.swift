5540d793104096375f071189526f82262ffa6448
Cal Stephens <cal.stephens@airbnb.com>
2025-04-03 09:14:40 -0700

    Update `blankLinesAtStartOfScope` rule to support switch cases and closures with captures/params (#1997)

--- Sources/Rules/BlankLinesAtStartOfScope.swift
+++ Sources/Rules/BlankLinesAtStartOfScope.swift
@@ -9,17 +9,14 @@
 import Foundation
 
 public extension FormatRule {
-    /// Remove blank lines immediately after an opening brace, bracket, paren or chevron
+    /// Remove blank lines immediately after an opening brace, bracket, paren, chevron, or colon
+    /// that starts a new scope.
     static let blankLinesAtStartOfScope = FormatRule(
         help: "Remove leading blank line at the start of a scope.",
         options: ["typeblanklines"]
     ) { formatter in
         formatter.forEach(.startOfScope) { i, token in
-            guard ["{", "(", "[", "<"].contains(token.string),
-                  let indexOfFirstLineBreak = formatter.index(of: .nonSpaceOrComment, after: i),
-                  // If there is extra code on the same line, ignore it
-                  formatter.tokens[indexOfFirstLineBreak].isLinebreak
-            else { return }
+            guard ["{", "(", "[", "<", ":"].contains(token.string) else { return }
 
             // Consumers can choose whether or not this rule should apply to type bodies
             if !formatter.options.removeStartOrEndBlankLinesFromTypes,
@@ -28,6 +25,23 @@ public extension FormatRule {
             {
                 return
             }
+
+            // If this is a closure with captures or params, skip to the `in` keyword
+            // before we look for a blank line at the start of the scope.
+            var startOfScope = i
+            if formatter.isStartOfClosure(at: startOfScope),
+               let endOfScope = formatter.endOfScope(at: startOfScope),
+               startOfScope + 1 < endOfScope,
+               let inKeywordIndex = formatter.index(of: .keyword("in"), in: startOfScope + 1 ..< endOfScope),
+               formatter.startOfScope(at: inKeywordIndex) == startOfScope
+            {
+                startOfScope = inKeywordIndex
+            }
+
+            guard let indexOfFirstLineBreak = formatter.index(of: .nonSpaceOrComment, after: startOfScope),
+                  // If there is extra code on the same line, ignore it
+                  formatter.tokens[indexOfFirstLineBreak].isLinebreak
+            else { return }
 
             // Find next non-space token
             var index = indexOfFirstLineBreak + 1

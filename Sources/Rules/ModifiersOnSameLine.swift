94b5a86efe6455a1b4b47eaff1b0a0b74677694b
Cal Stephens <cal@calstephens.tech>
2025-05-30 12:15:03 -0700

    Add new rule to keep declaration modifiers on same line (#2073)

---
+++ Sources/Rules/ModifiersOnSameLine.swift
@@ -0,0 +1,80 @@
+//
+//  ModifiersOnSameLine.swift
+//  SwiftFormat
+//
+//  Created by cal_stephens on 5/29/25.
+//  Copyright © 2025 Nick Lockwood. All rights reserved.
+//
+
+import Foundation
+
+public extension FormatRule {
+    /// Ensure all modifiers are on the same line as the declaration keyword
+    static let modifiersOnSameLine = FormatRule(
+        help: "Ensure that all modifiers are on the same line as the declaration keyword."
+    ) { formatter in
+        formatter.forEachToken { i, token in
+            // Check if this is a declaration keyword
+            guard token.isDeclarationTypeKeyword else { return }
+
+            // Find the start of modifiers (excluding attributes)
+            let modifierStart = formatter.startOfModifiers(at: i, includingAttributes: false)
+
+            // If there are no modifiers before the declaration, nothing to do
+            guard modifierStart < i else { return }
+
+            // Check if modifiers and declaration are already on the same line
+            if formatter.onSameLine(modifierStart, i) {
+                return
+            }
+
+            // Check if there are any comments between modifiers and declaration
+            // If there are, we should preserve the existing formatting
+            var hasComment = false
+            for index in modifierStart ..< i {
+                if formatter.tokens[index].isComment {
+                    hasComment = true
+                    break
+                }
+            }
+
+            if hasComment {
+                return
+            }
+
+            // Unwrap all lines between modifiers and the declaration
+            var currentIndex = i
+            while currentIndex > modifierStart {
+                guard let prevIndex = formatter.index(of: .nonSpaceOrLinebreak, before: currentIndex) else {
+                    break
+                }
+
+                // If there's a linebreak between previous and current token, unwrap it
+                if formatter.tokens[prevIndex + 1 ..< currentIndex].contains(where: \.isLinebreak) {
+                    _ = formatter.unwrapLine(before: currentIndex, preservingComments: true)
+                }
+
+                currentIndex = prevIndex
+            }
+        }
+    } examples: {
+        """
+        ```diff
+        - @MainActor
+        - public
+        - private(set)
+        - var foo: Foo
+
+        + @MainActor
+        + public private(set) var foo: Foo
+        ```
+
+        ```diff
+        - nonisolated
+        - func bar() {}
+
+        + nonisolated func bar() {}
+        ```
+        """
+    }
+}
---
+++ Tests/Rules/ModifiersOnSameLineTests.swift
@@ -0,0 +1,174 @@
+//
+//  ModifiersOnSameLineTests.swift
+//  SwiftFormatTests
+//
+//  Created by cal_stephens on 5/29/25.
+//  Copyright © 2025 Nick Lockwood. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftFormat
+
+class ModifiersOnSameLineTests: XCTestCase {
+    // MARK: - modifiersOnSameLine
+
+    func testModifiersOnSeparateLinesAreCombined() {
+        let input = """
+        public
+        private(set)
+        var foo: Foo
+        """
+        let output = """
+        public private(set) var foo: Foo
+        """
+        testFormatting(for: input, output, rule: .modifiersOnSameLine)
+    }
+
+    func testSingleModifierOnSeparateLineIsCombined() {
+        let input = """
+        public
+        var foo: Foo
+        """
+        let output = """
+        public var foo: Foo
+        """
+        testFormatting(for: input, output, rule: .modifiersOnSameLine)
+    }
+
+    func testNonisolatedModifierOnSeparateLineIsCombined() {
+        let input = """
+        nonisolated
+        func bar() {}
+        """
+        let output = """
+        nonisolated func bar() {}
+        """
+        testFormatting(for: input, output, rule: .modifiersOnSameLine)
+    }
+
+    func testMultipleModifiersOnMultipleLinesAreCombined() {
+        let input = """
+        class Container {
+            public
+            static
+            final
+            var foo: String = ""
+        }
+        """
+        let output = """
+        class Container {
+            public static final var foo: String = ""
+        }
+        """
+        testFormatting(for: input, output, rule: .modifiersOnSameLine, exclude: [.modifierOrder])
+    }
+
+    func testAttributesCanRemainOnSeparateLines() {
+        let input = """
+        @MainActor
+        public var foo: Foo
+        """
+        testFormatting(for: input, rule: .modifiersOnSameLine)
+    }
+
+    func testAttributesOnSeparateLinesWithModifiersOnSeparateLines() {
+        let input = """
+        @MainActor
+        public
+        private(set)
+        var foo: Foo
+        """
+        let output = """
+        @MainActor
+        public private(set) var foo: Foo
+        """
+        testFormatting(for: input, output, rule: .modifiersOnSameLine)
+    }
+
+    func testMultipleAttributesCanRemainOnSeparateLines() {
+        let input = """
+        @MainActor
+        @Published
+        public var foo: Foo
+        """
+        testFormatting(for: input, rule: .modifiersOnSameLine)
+    }
+
+    func testModifiersAlreadyOnSameLineAreNotChanged() {
+        let input = """
+        public private(set) var foo: Foo
+        """
+        testFormatting(for: input, rule: .modifiersOnSameLine)
+    }
+
+    func testCommentsArePreserved() {
+        let input = """
+        public
+        // This is private setter
+        private(set)
+        var foo: Foo
+        """
+        testFormatting(for: input, rule: .modifiersOnSameLine, exclude: [.docComments, .docCommentsBeforeModifiers])
+    }
+
+    func testDeclarationWithoutModifiersIsNotChanged() {
+        let input = """
+        var foo: Foo
+        func bar() {}
+        class Baz {}
+        """
+        testFormatting(for: input, rule: .modifiersOnSameLine)
+    }
+
+    func testOnlyAttributesWithoutModifiers() {
+        let input = """
+        @MainActor
+        var foo: Foo
+        """
+        testFormatting(for: input, rule: .modifiersOnSameLine)
+    }
+
+    func testModifiersInStructDeclaration() {
+        let input = """
+        public
+        struct MyStruct {
+            private
+            var value: Int
+        }
+        """
+        let output = """
+        public struct MyStruct {
+            private var value: Int
+        }
+        """
+        testFormatting(for: input, output, rule: .modifiersOnSameLine)
+    }
+
+    func testModifiersInProtocolDeclaration() {
+        let input = """
+        public
+        protocol MyProtocol {
+            static
+            func someMethod()
+        }
+        """
+        let output = """
+        public protocol MyProtocol {
+            static func someMethod()
+        }
+        """
+        testFormatting(for: input, output, rule: .modifiersOnSameLine)
+    }
+
+    func testModifiersWithComplexAccessControl() {
+        let input = """
+        public
+        private(set)
+        var complexProperty: String
+        """
+        let output = """
+        public private(set) var complexProperty: String
+        """
+        testFormatting(for: input, output, rule: .modifiersOnSameLine, exclude: [.redundantInternal, .redundantFileprivate, .modifierOrder])
+    }
+}
